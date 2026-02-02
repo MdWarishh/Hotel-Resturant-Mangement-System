a21f298f4aa8091ce7d269d2c8876ada857764d5
MdWarishh <171806296+MdWarishh@users.noreply.github.com>
2026-02-02 18:55:25 +0530

    updated

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api';
+  process.env.NEXT_PUBLIC_API_BASE_URL;
 
 /**
  * Central Axios Instance
  * Used across the entire frontend
  */
 const api = axios.create({
-  baseURL: API_BASE_URL || process.env.NEXT_PUBLIC_API_URL,
+  // API_BASE_URL ||
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
